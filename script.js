f12bf5c5cf99942fd9c7190fffd02b72aa29ea60
TommyGC11 <tommycontreras34@gmail.com>
2025-01-07 10:31:59 -0400

    feat: :sparkles: add function to play the previous song

--- script.js
+++ script.js
@@ -122,6 +122,16 @@ const playNextSong = () => {
   }
 };
 
+const playPreviousSong = () => {
+  if (userData?.currentSong === null) return;
+  else {
+   const currentSongIndex = getCurrentSongIndex();
+   const previousSong = userData?.songs[currentSongIndex - 1];
+
+   playSong(previousSong.id);
+  }
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
