84bdc1c9f1506b1e68e0d7fc21faad89a642b393
TommyGC11 <tommycontreras34@gmail.com>
2025-01-07 10:33:10 -0400

    feat: :sparkles: add function to display the title and song artist of the song which is being played

--- script.js
+++ script.js
@@ -132,6 +132,16 @@ const playPreviousSong = () => {
   }
 };
 
+const setPlayerDisplay = () => {
+  const playingSong = document.getElementById("player-song-title");
+  const songArtist = document.getElementById("player-song-artist");
+  const currentTitle = userData?.currentSong?.title;
+  const currentArtist = userData?.currentSong?.artist;
+
+  playingSong.textContent = currentTitle ? currentTitle : "";
+  songArtist.textContent = currentArtist ? currentArtist : "";
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
