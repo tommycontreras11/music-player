8c57c2f86db6ee047c55d4a547fb87250e23af13
TommyGC11 <tommycontreras34@gmail.com>
2025-01-07 10:34:23 -0400

    feat: :sparkles: add a function to add a highlight when a song is played

--- script.js
+++ script.js
@@ -142,6 +142,19 @@ const setPlayerDisplay = () => {
   songArtist.textContent = currentArtist ? currentArtist : "";
 };
 
+const highlightCurrentSong = () => {
+  const playlistSongElements = document.querySelectorAll(".playlist-song");
+  const songToHighlight = document.getElementById(
+    `song-${userData?.currentSong?.id}`
+  );
+
+  playlistSongElements.forEach((songEl) => {
+    songEl.removeAttribute("aria-current");
+  });
+
+  if (songToHighlight) songToHighlight.setAttribute("aria-current", "true");
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
