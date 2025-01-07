d717d301ea9971a1995a8e4ab86a7dc93f4f2e2a
TommyGC11 <tommycontreras34@gmail.com>
2025-01-07 10:30:35 -0400

    feat: :sparkles: add function to play song

--- script.js
+++ script.js
@@ -85,6 +85,25 @@ let userData = {
   songCurrentTime: 0,
 };
 
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
+
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  } else {
+    audio.currentTime = userData?.songCurrentTime;
+  }
+  userData.currentSong = song;
+  playButton.classList.add("playing");
+
+  highlightCurrentSong();
+  setPlayerDisplay();
+  setPlayButtonAccessibleText();
+  audio.play();
+};
+
 const renderSongs = (array) => {
   const songsHTML = array
     .map((song) => {
